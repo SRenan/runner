de07356233160bedb948534c90aeb7ac9cbca30d
Renan Sauteraud <renan.sauteraud@gmail.com>
2024-06-18 09:22:04 -0700

    Add scrolling background.
    Set camera to fixed position.
    Add pause button.
    Propagate character choice to RunScene.

--- runner.js
+++ runner.js
@@ -2,7 +2,6 @@
 Character is fixed
 Obstacles have negative velocity
 --------------------------------------------------*/
-
 class PreloadScene extends Phaser.Scene{
     //Preload everything then automatically transition to the MainMenuScene
     constructor(){
@@ -10,6 +9,7 @@ class PreloadScene extends Phaser.Scene{
     }
     preload(){
         this.load.image('sky', 'assets/sky.png');
+        this.load.image("sky2", "assets/sky2.png");
         this.load.image('ground', 'assets/platform.png');
         this.load.image('star', 'assets/star.png');
         this.load.image('bomb', 'assets/bomb.png');
@@ -17,6 +17,12 @@ class PreloadScene extends Phaser.Scene{
             'assets/dude.png',
             { frameWidth: 32, frameHeight: 48 }
         );
+        this.load.spritesheet('dude_red',
+            'assets/dude_red.png',
+            { frameWidth: 32, frameHeight: 48 }
+        );
+        this.load.image('dude1', 'assets/star.png');
+        this.load.image('dude2', 'assets/bomb.png');
     }
     create(){
         //Check whether we have a previous run (This should likely wrapped into one big variable if there is more to save)
@@ -30,6 +36,7 @@ class PreloadScene extends Phaser.Scene{
     update(){
     }
 }
+
 class MainMenuScene extends Phaser.Scene{
     constructor(){
         super('MainMenuScene');
@@ -50,6 +57,32 @@ class MainMenuScene extends Phaser.Scene{
     update(){
     }
 }
+
+class HUDScene extends Phaser.Scene{
+    constructor(){
+        super('HUDScene');
+    }
+    create(){
+        const goToMenu = this.add.text(0, 40, 'HUD to Menu', { fill: '#2e2d8c' });
+        goToMenu.setInteractive();
+        goToMenu.on('pointerdown', () => {
+          this.scene.stop('HUDScene');
+          this.scene.stop('RunScene');
+          this.scene.start('MainMenuScene');
+        });
+    }
+}
+
+class SettingsScene extends Phaser.Scene{
+    constructor(){
+        super('SettingsScene');
+    }
+    create(){
+        const volumeSlider = this.rexUI.add.slider({
+        });
+    }
+}
+
 class RunScene extends Phaser.Scene{
     constructor(){
         super('RunScene');
@@ -59,28 +92,32 @@ class RunScene extends Phaser.Scene{
     create(){
         this.add.image(400, 300, 'sky');
 
+        //Parallax
+        this.background = this.add.tileSprite(0, 0, config.width, 256, "sky2")
+            .setOrigin(0)
+            .setScrollFactor(0, 1); //this line keeps your background from scrolling outside of camera bounds
         //"this" required for every variable in the class
         this.ground = this.physics.add.staticGroup();
         this.ground.create(400, 568, 'ground').setScale(2).refreshBody();
         this.platforms = this.physics.add.group({immovable: true, allowGravity: false});
 
-        this.player = this.physics.add.sprite(100, 450, 'dude');
+        this.player = this.physics.add.sprite(100, 450, player_config.sprite_key);
         this.player.setBounce(0.2);
         this.player.setCollideWorldBounds(true);
         this.anims.create({
             key: 'left',
-            frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+            frames: this.anims.generateFrameNumbers(player_config.sprite_key, { start: 0, end: 3 }),
             frameRate: 10,
             repeat: -1
         });
         this.anims.create({
             key: 'turn',
-            frames: [ { key: 'dude', frame: 4 } ],
+            frames: [ { key: player_config.sprite_key, frame: 4 } ],
             frameRate: 20
         });
         this.anims.create({
             key: 'right',
-            frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+            frames: this.anims.generateFrameNumbers(player_config.sprite_key, { start: 5, end: 8 }),
             frameRate: 10,
             repeat: -1
         });
@@ -112,8 +149,12 @@ class RunScene extends Phaser.Scene{
         this.physics.add.overlap(this.player, this.stars, this.collectStar, null, this);
         /*--------------- controls and cameras --------------*/
         this.cursors = this.input.keyboard.createCursorKeys();
-        this.cameras.main.startFollow(this.player);
-        this.cameras.main.setLerp(1,0);
+        //this.cameras.main.startFollow(this.player);
+        //this.cameras.main.followOffset.x = -300;
+        //this.cameras.main.followOffset.y = 200;
+        //this.cameras.main.setLerp(1,0.001);
+        this.keyA = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
+        this.keyESC = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.ESC);
 
         /*-------------------- Interface ----- --------------*/
         const goToMenu = this.add.text(0, 0, 'Go to Menu', { fill: '#2e2d8c' });
@@ -121,18 +162,38 @@ class RunScene extends Phaser.Scene{
         goToMenu.on('pointerdown', () => {
           this.scene.start('MainMenuScene');
         });
+        this.scene.launch('HUDScene');
 
         this.coins = []; //Create them as an array and add/rm members
         this.money = this.registry.get("money");
         this.moneyText = this.add.text(16, 16, '$$: '+this.money, { fontSize: '32px', fill: '#000' });
         this.testText = this.add.text(400, 300, 'TEXT', { fontSize: '32px', fill: '#000' });
         this.moneyText.setScrollFactor(0);
+
+        this.paused = false;
     }
     update(time, delta){
         this.player.anims.play('right', true);
+        //this.background.setTilePosition(100);
+        this.background.tilePositionX += 1;
+
+        /*-------------------- Controls --------------------*/
         if (this.cursors.up.isDown && this.player.body.touching.down){
             this.player.setVelocityY(-330);
         }
+        if (this.keyESC.isDown){
+           //TODO: THis is messy. Should probably make a GameMenuScene that handles resuming.
+            this.paused = !this.paused;
+              console.log(this.paused);
+            if(!this.paused){
+              this.scene.pause("RunScene");
+              this.scene.launch("PauseScene");
+            } else{
+              this.scene.resume("RunScene");
+            }
+        }
+        
+        /*-------------------- Timers --------------------*/
         this.timer += delta;
         if(this.timer >= 3000){
             this.createBomb();
@@ -146,7 +207,7 @@ class RunScene extends Phaser.Scene{
     }
     createPlatform(){
         const randY = Phaser.Math.Between(20, 50)*10;
-        const platform = this.platforms.create(600, randY, 'ground');
+        const platform = this.platforms.create(1000, randY, 'ground');
         platform.setVelocityX(-100);
     }
     collectStar(player, star){
@@ -163,6 +224,7 @@ class RunScene extends Phaser.Scene{
     }
     gameOver(){
         this.registry.set("money", this.money);
+        this.scene.stop('HUDScene');
         this.time.delayedCall(2000, () => this.scene.start('GameOverScene')); //After 2s, go to GameOver screen
         localStorage.setItem('money', this.money);
     }
@@ -180,8 +242,32 @@ class GarageScene extends Phaser.Scene{
         });
         this.money = this.registry.get("money");
         this.moneyText = this.add.text(16, 16, '$$: '+this.money, { fontSize: '32px', fill: '#000' });
+
+        /*---------------------- Choose runner ---------------*/
+        // Display a dynamic list of available outfits.
+        // Display a list of outfits that can be bought.
+        // Display currently selected.
+        this.player = this.physics.add.sprite(100, 450, player_config.sprite_key);
+        this.player.setCollideWorldBounds(true);
+
+        const setSkin1 = this.add.text(400, 100, 'Skin1', { fill: '#2e2d8c' });
+        setSkin1.setInteractive();
+        setSkin1.on('pointerdown', () => {
+          player_config.sprite_key = 'dude'
+          this.player.setTexture(player_config.sprite_key);
+        });
+        const setSkin2 = this.add.text(400, 200, 'Skin2', { fill: '#2e2d8c' });
+        setSkin2.setInteractive();
+        setSkin2.on('pointerdown', () => {
+          player_config.sprite_key = 'dude_red'
+          this.player.setTexture(player_config.sprite_key);
+        });
+        
+    }
+    update(){
     }
 }
+
 class GameOverScene extends Phaser.Scene{
     //Summary of run and return to MainMenuScene
     constructor(){
@@ -197,6 +283,7 @@ class GameOverScene extends Phaser.Scene{
     }
 }
 
+
 var config = {
     type: Phaser.AUTO,
     width: 800,
@@ -209,7 +296,15 @@ var config = {
             debug: false
         }
     },
-    scene: [PreloadScene, MainMenuScene, RunScene, GarageScene, GameOverScene]
+    settings: {
+      volume: 1
+    },
+    scene: [PreloadScene, MainMenuScene, SettingsScene, HUDScene, RunScene, GarageScene, GameOverScene]
+};
+
+var player_config = {
+    player_name: 'player',
+    sprite_key: 'dude'
 };
 
 var game = new Phaser.Game(config);
